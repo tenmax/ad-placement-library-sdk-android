a2838c374ce76d132074646b6c6e504d8f5593e4
Spirit <dbi1463@gmail.com>
2024-11-01 18:32:28 +0800

    feat: change to use buttons to show different ADs

--- sdkdemo/src/main/java/io/tenmax/sdkdemo/ui/home/HomeFragment.java
+++ sdkdemo/src/main/java/io/tenmax/sdkdemo/ui/home/HomeFragment.java
@@ -11,13 +11,17 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
 import io.tenmax.mobilesdk.TenMaxAd;
+import io.tenmax.sdkdemo.R;
 import io.tenmax.sdkdemo.SupportedSpaces;
 import io.tenmax.sdkdemo.databinding.FragmentHomeBinding;
 import io.tenmax.sdkdemo.ui.SimpleAdSessionListener;
 import io.tenmax.sdkdemo.ui.SimpleInitiationCallback;
+import io.tenmax.sdkdemo.ui.dashboard.DashboardFragment;
+import io.tenmax.sdkdemo.ui.notifications.NotificationsFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -30,18 +34,27 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         SimpleAdSessionListener listener = new SimpleAdSessionListener(this.getContext());
         SimpleInitiationCallback callback = new SimpleInitiationCallback(this.getContext());
         this.fullscreenAd = interstitialAd(SupportedSpaces.interstitialId, this.getActivity(), listener, callback);
+        this.binding.showInterstitialAd.setOnClickListener((view) -> {
+            this.fullscreenAd.show();
+        });
+
+        this.binding.showInlineAd.setOnClickListener((view) -> {
+            Fragment dashboard = new DashboardFragment();
+            this.pushFragment(dashboard);
+        });
+        this.binding.showBannerAd.setOnClickListener((view) -> {
+            Fragment notifications = new NotificationsFragment();
+            this.pushFragment(notifications);
+        });
         return root;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        this.fullscreenAd.show();
     }
 
     @Override
@@ -49,5 +62,14 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
         cleanAd(this.fullscreenAd);
+    }
+
+    private void pushFragment(Fragment fragment) {
+        FragmentManager manager = getParentFragmentManager();
+        manager.beginTransaction()
+            .add(R.id.fragmentContainer, fragment)
+            .hide(this)
+            .addToBackStack("home")
+            .commit();
     }
 }
